ddbfc1872e1e205439811b780cb7436722ec4134
Abayomi igwubor <igwubor@outlook.com>
2019-09-08 16:10:13 +0100

    clockApp

--- src/Components/Clock.jsx
+++ src/Components/Clock.jsx
@@ -3,9 +3,21 @@ import React from 'react';
 class Clock extends React.Component {
 	constructor(props) {
 		super(props);
-		this.state = {
+		this.updateClock = this.updateClock.bind(this);
+	}
+	state = {
+		time : new Date().toLocaleString()
+	};
+	componentDidMount() {
+		this.intervalID = setInterval(this.updateClock, 1000);
+	}
+	componentWillUnmount() {
+		clearInterval(this.intervalID);
+	}
+	updateClock() {
+		this.setState({
 			time : new Date().toLocaleString()
-		};
+		});
 	}
 	render() {
 		return (
